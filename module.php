2c73fe8ffe9111a04506ad42623be10e2b895349
unknown <pvitaly91@gmail.com>
2018-08-18 23:31:12 +0100

    Added others features

--- module.php
+++ module.php
@@ -3,13 +3,12 @@
 /**
  *  Implements hook_menu
  */
-
 function tr_form_menu() {
     $items['learn/tr-form'] = [
         'title' => t('TerraSoft'),
         'page callback' => 'drupal_get_form',
         //   'page callback' => "tr_form_html_page",
-        'page arguments' => ['first_form'],
+        'page arguments' => ['domain_form'],
         'access callback' => TRUE,
         'type' => MENU_NORMAL_ITEM
     ];
@@ -17,16 +16,25 @@ function tr_form_menu() {
 }
 
 function getCountries($id = NULL) {
-   
-    if (isset($countries[$id])) {
-        return $countries[$id];
-    }else{
-      //  $countries = db_select("tr_form_countries")->ge//>addField("name")
+
+    if (isset($id)) {
+        //  return $countries[$id];
+        $result = db_select("tr_form_countries", "t")->fields("t", ["name"])->condition("cid", $id)->execute()->fetchAll();
+        if(isset($result[0])){
+            $countries = $result[0]->name;
+        }
+    } else {
+        $result = db_select("tr_form_countries", "t")->fields("t", ["name", "cid"])->execute()->fetchAllAssoc('cid');
+        if (is_array($result)) {
+            foreach ($result as $cid => $item) {
+                $countries[$cid] = $item->name;
+            }
+        }
     }
     return $countries;
 }
 
-function first_form() {
+function domain_form() {
     /*
       TOODO Country array must take from DB or Another source
 
@@ -44,7 +52,9 @@ function first_form() {
         '#type' => 'select',
         '#options' => getCountries()
     );
-
+    $form['city'] = array(
+        '#type' => 'textfield',
+        '#attributes' => array('placeholder' => t('City')),);
     $form['code'] = array(
         '#type' => 'textfield',
         '#attributes' => array('placeholder' => t('Code')),
@@ -55,20 +65,22 @@ function first_form() {
     );
     $form['submit'] = array(
         '#type' => 'submit',
-        '#value' => 'Submit',
+        '#value' => 'Register',
     );
 
     return $form;
 }
-function getDomainZone(){
+
+function getDomainZone() {
     return "bpmonline.com";
 }
+
 /**
  *  Implements hook_theme()
  */
 function tr_form_theme($existing, $type, $theme, $path) {
 
-    $items['first_form'] = array(
+    $items['domain_form'] = array(
         'render element' => 'form',
         'template' => 'form',
         'path' => drupal_get_path('module', 'tr_form') . '/template/form',
@@ -77,27 +89,56 @@ function tr_form_theme($existing, $type, $theme, $path) {
     return $items;
 }
 
-function first_form_validate($form, &$form_state) {
+function domain_form_validate($form, &$form_state) {
     // form_set_error('zip',t('sasasa'));
 }
 
-function first_form_submit($form, &$form_state) {
-    //exit;
+function domain_form_submit($form, &$form_state) {
+   // kint($form_state);
+    /* add new domain */
     $domain = $insert = [];
-    $domain["name"] = $form_state["values"]["domain"].".".getDomainZone();
+    $domain["name"] = $form_state["values"]["domain"] . "." . getDomainZone();
     $domain_id = db_insert("tr_form_domains")->fields($domain)->execute();
-    
-    
-    $insert["phone"] = $form_state["values"]["code"].$form_state["values"]["phone"];
-  
+
+    /*
+    *  Add form result
+    *  making array of fileds for insert to DB
+    */
+    $insert["phone"] = $form_state["values"]["code"] . $form_state["values"]["phone"];
     $insert["country_id"] = $form_state["values"]["country_id"];
+    isset($form_state["values"]["city"]) ? $insert["city"] = $form_state["values"]["city"] : true;
     $insert["login"] = $form_state["values"]["login"];
     $insert["domain_id"] = $domain_id;
-   
+    
+    //inserting new form result to DB
     db_insert("tr_form_result")->fields($insert)->execute();
+    $insert["domain"] = $domain["name"];
+    $insert["country"] = getCountries($insert["country_id"]);
+    unset($insert["domain_id"]);
+    unset($insert["country_id"]);
+    
+    //sending mail to site administrator email
+    drupal_mail("tr_form", "domain_form", variable_get("site_mail"), language_default(), $insert);
     drupal_set_message(t("success"));
-
 }
 
+/**
+ *  Implements hook_mail()
+ */
+function tr_form_mail($key, &$message, $params) {
 
-
+    switch ($key) {
+        case 'domain_form':
+            // Set headers etc
+            $msg = "";
+            foreach ($params as $f_name => $f_value) {
+                if (isset($f_value)){
+                    $p["@" . $f_name] = $f_value;
+                    $msg .= "<strong>".ucfirst($f_name).":</strong> ".$f_value."<br>";
+                }    
+            }          
+            $message['subject'] = t('Form registration:');
+            $message['body'][] = t('Hello you have new message from form registration:<br> '.$msg, $p);
+            break;
+    }
+}
--- template/form/form.tpl.php
+++ template/form/form.tpl.php
@@ -1,44 +1,205 @@
-<?php
-kint($variables);
+<?
+//kint($variables);
+global $base_path;
+?>
+<style>
+    @font-face {
+        font-family: 'Helvetica Neue Cyr Thin';
+        src: url('/sites/all/modules/tr_form/fonts/HelveticaNeueCyr-Thin.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+    }
+    .form{ font-family: "Helvetica Neue Cyr Thin";
+    }
 
-?><div class="content">
-    <form action="/learn/tr-form" method="post" id="<?=$variables["form"]['#form_id']?>" accept-charset="UTF-8">
-        <div>
-            <? foreach($variables["form"] as $f_name => $f):?>
-                <?  if(is_array($f) && isset($f['#type'])):?>
-                    <? if($f['#type'] == "textfield" ):?>
-                        <div class="form-item form-type-textfield form-item-domain">
-                            <input 
-                               
-                                type="text"
-                                id="edit-<?=$f_name?>"
-                                name="<?=$f_name?>"
-                                value="<?=$f["#value"]?>" 
-                                size="<?=$f['#size']?>" 
-                                maxlength="<?=$f["#maxlength"]?>" 
-                                <?if(is_array($f['#attributes'])):
-                                    foreach($f['#attributes'] as $attr_name => $attr_value):
-                                        echo " ".$attr_name."=".$attr_value;
-                                    endforeach;
-                               endif;?>
-                                class="form-text">
+    .from-container form{
+        /*padding:0px 40px 20px;*/
+        margin:0px;
+
+        text-align:center;
+        color:white;
+    }
+    .from-container form a{
+        color:white;
+    }
+    .from-container{
+        padding:40px;
+    }
+
+    .from-container input, .from-container .tr-form-select{
+        background-color:transparent;
+        border:none;
+        border-bottom:1px solid white;
+        border-radius:0px;
+    }
+    .from-container{
+
+        background-color:#64b8df;
+        border-width: 0.5px;
+        border-color: rgb(149, 149, 149);
+        border-style: solid;
+        border-radius: 6px;
+    }
+    .from-container p {
+        color:white;
+        font-size: 26px;
+        line-height: 0.714;
+        text-align: center;
+    }
+    p.title {
+        font-size: 25px;
+
+        color: rgb(94, 100, 100);
+        margin-bottom:30px;
+        text-align: center;
+
+    }
+
+    button {
+        font-size: 22px;
+        color: rgb(255, 255, 255);
+        line-height: 0.909;
+        text-align: center;
+
+        border-radius: 4px;
+        background-color: #f27b30;
+        width:100%;
+
+    }
+    .links{
+        color:#5e6464;
+        padding-top:20px;
+        text-align:center;
+    }
+    .links a {
+        margin:0px 20px;
+        color:#5e6464;
+    }
+    .form-group div.domain{
+        margin:0px;
+        padding:15px 0px 0px;
+        text-align:left;
+        color:white;
+    }
+    .tr-form-select{
+        
+        color: #b2dcef;
+    }
+    input[type="text"]::-webkit-input-placeholder { color: #b2dcef; }
+    input[type="text"]:-ms-input-placeholder { color: #b2dcef; }
+    input[type="text"]::-ms-input-placeholder { color: #b2dcef; }
+    input[type="text"]::placeholder { color: #b2dcef; } 
+
+</style>
+<? kint($variables["form"]);?>
+<div class="row">
+    <div class="col-sm-12 form">
+        <div class="form">
+          
+            <p class="title"><?=drupal_get_title();?></p>
+            <div class="from-container">
+                <p><?=t('Register for a 14-day Free Trial')?></p>
+                <form action="/learn/tr-form" method="post" id="<?= $variables["form"]['#form_id'] ?>" accept-charset="UTF-8">
+                    <? foreach ($variables["form"] as $f_name => $f): ?>
+                        <? if (is_array($f) && isset($f['#type'])): ?>
+                            <? if ($f['#type'] == "textfield" && $f_name != "domain" && $f_name != "code" && $f_name != "phone"): ?>
+                                <div class="form-group">
+                                    <input 
+                                        type="text"
+                                        id="edit-<?= $f_name ?>"
+                                        name="<?= $f_name ?>"
+                                        value="<?= $f["#value"] ?>" 
+                                        maxlength="<?= $f["#maxlength"] ?>" 
+                                        <?
+                                        if (is_array($f['#attributes'])):
+                                            foreach ($f['#attributes'] as $attr_name => $attr_value):
+                                                echo " " . $attr_name . "=" . $attr_value;
+                                            endforeach;
+                                        endif;
+                                        ?>
+                                        class="form-text form-control">
+                                </div>
+                            <? elseif($f_name == "domain"):?>    
+                                <div class="form-group row">
+                                    <div class="col-sm-7 ">
+                                        <input 
+                                            type="text"
+                                            id="edit-<?= $f_name ?>"
+                                            name="<?= $f_name ?>"
+                                            value="<?= $f["#value"] ?>" 
+                                            maxlength="<?= $f["#maxlength"] ?>" 
+                                            <?
+                                            if (is_array($f['#attributes'])):
+                                                foreach ($f['#attributes'] as $attr_name => $attr_value):
+                                                    echo " " . $attr_name . "=" . $attr_value;
+                                                endforeach;
+                                            endif;
+                                            ?>
+                                            class="form-text form-control">
+                                    </div>
+                                    <div class="col-sm-5 domain">
+                                        <?=getDomainZone()?>
+                                    </div>
+                                </div>
+                             <? elseif ($f['#type'] == "select" && is_array($f['#options'])): ?>
+                                <div class="form-group form-item form-type-select form-item-country">
+                                    <select id="edit-country" name="<?= $f_name ?>" class="custom-select tr-form-select" >
+                                        <? foreach ($f['#options'] as $id => $value): ?>
+                                            <option 
+                                                value="<?= $id ?>"
+                                                <? ($f['#value'] == $id) ? print("selected") : false; ?> 
+                                             ><?= $value ?></option>
+                                        <? endforeach; ?>
+                                    </select>
+                                </div>
+                            <? endif; ?>
+                        <? endif; ?>
+                    <? endforeach; ?>
+                    <?  /*
+                        *   code and phone fields
+                        */ ?>
+                        <div class=" form-group row">
+                            <div class="col-sm-3 ">
+                                 <input 
+                                    type="text"
+                                    id="edit-code"
+                                    name="code"
+                                    value="<?= $variables["form"]["code"]["#value"] ?>" 
+                                    <? if (is_array($variables["form"]["code"]['#attributes'])):
+                                        foreach ($variables["form"]["code"]['#attributes'] as $attr_name => $attr_value):
+                                            echo " " . $attr_name . "=" . $attr_value;
+                                        endforeach;
+                                    endif;?>
+                                    class="form-text form-control">
+                            </div>	
+                            <div class="col-sm-9 ">
+                                <input 
+                                    type="text"
+                                    id="edit-phone"
+                                    name="phone"
+                                    value="<?= $variables["form"]["phone"]["#value"] ?>" 
+                                    <?if (is_array($variables["form"]["phone"]['#attributes'])):
+                                        foreach ($variables["form"]["phone"]['#attributes'] as $attr_name => $attr_value):
+                                            echo " " . $attr_name . "=" . $attr_value;
+                                        endforeach;
+                                    endif;?>
+                                    class="form-text form-control">
+                            </div>
                         </div>
-                   <? elseif($f['#type'] == "select" && is_array($f['#options'])):?>
-                        <div class="form-item form-type-select form-item-country">
-                           
-                            <select id="edit-country" name="<?=$f_name?>" class="form-select">
-                                <? foreach ($f['#options'] as $id => $value):?>
-                                    <option value="<?=$id?>" <? ($f['#value'] == $id) ? print("selected") : false; ?> ><?=$value?></option>
-                                <? endforeach;?>
-                            </select>
-                        </div>
-                    <? endif?>
-                <? endif?>
-            <? endforeach;?>
-            <input type="submit" id="edit-submit" name="op" value="<?=$variables["form"]['submit']["#value"]?>" class="form-submit">
-            <input type="hidden" name="form_build_id" value="<?=$variables["form"]['form_build_id']["#value"]?>">
-            <input type="hidden" name="form_token" value="<?=$variables["form"]["form_token"]["#value"]?>">
-            <input type="hidden" name="form_id" value="<?=$variables["form"]['form_id']["#value"]?>">
+                   
+                    <div class="form-group">
+                        <button type="submit" id="edit-submit" name="op" class="btn "><?= $variables["form"]['submit']["#value"] ?></button>
+                        <? if(isset($variables["form"]["form_build_id"]["#value"])):?><input type="hidden" name="form_build_id" value="<?= $variables["form"]['form_build_id']["#value"] ?>"><? endif;?>
+                        <? if(isset($variables["form"]["form_token"]["#value"])):?><input type="hidden" name="form_token" value="<?= $variables["form"]["form_token"]["#value"] ?>"><? endif;?>
+                        <? if(isset($variables["form"]["form_id"]["#value"])):?><input type="hidden" name="form_id" value="<?= $variables["form"]['form_id']["#value"] ?>"><? endif;?>
+                    </div>
+                    <hr>
+                    <a href="<?=$base_path?>user/register"><? user_is_logged_in() ? print(t('Edit Profile')) :  print(t('Create Account'))?></a>
+                </form>
+            </div>
+            <div class="links">
+                <a href="<?=$base_path?>"><?=t('Main Page')?></a> | <a href="<?=$base_path?>contacts"><?=t('Contacts')?></a>
+            </div>
         </div>
-    </form> 
+    </div>
 </div>
